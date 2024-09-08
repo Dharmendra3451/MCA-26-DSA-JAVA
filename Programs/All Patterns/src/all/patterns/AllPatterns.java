18c3e6d6edf0790c2f1c1cede61f6b8b5863c64a
Dharmendra Rathore <rathoredharmendra3451@gmail.com>
2024-09-08 16:17:36 +0530

    COMMENT ADDED

--- Programs/All Patterns/src/all/patterns/AllPatterns.java
+++ Programs/All Patterns/src/all/patterns/AllPatterns.java
@@ -7,6 +7,8 @@ public class AllPatterns {
     public static void main(String[] args) {
        int n=4;
        
+       
+    // For Loop Inits       
       for(int i = 1; i <= n;  i++)
       {
          for(int j = 1; j <= n; j++){
